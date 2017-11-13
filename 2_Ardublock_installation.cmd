fedc4acb8024cc836d394c1a82f20d9c28a3b6e8
SebCanet <scanet@libreduc.cc>
2017-11-13 23:33:07 +0100

    update folders

--- 2_Ardublock_installation.cmd
+++ 2_Ardublock_installation.cmd
@@ -5,6 +5,15 @@ SET currentpath=%~dp1
 cd %currentpath%portable\ardublock
 cls
 xcopy ArduBlock*.* /S/C ..\..\tools
-xcopy Matrice\* /S/C ..\..\tools\Matrice\
-xcopy Molegraph\* /S/C ..\..\tools\Molegraph\
-xcopy bibliotheques_DuinoEdu\* /S/C ..\sketchbook\libraries\
+pause
+xcopy "..\sketchbook\6.complements\DuinoEdu_bibliotheques" /S/C ..\sketchbook\libraries
+pause
+xcopy "4.DuinoEDU - Ardublock" /S/C/I "..\sketchbook\4.DuinoEDU - Ardublock"
+pause
+rd /s/q "..\sketchbook\4.TechnoZone51 - Organigram"
+pause
+rd /s/q "..\sketchbook\6.complements\DuinoEdu_bibliotheques"
+pause
+cd ..
+rd /s/q ardublock
+pause
