14436341e36e75b126da86e09a84d179cfe8597f
SebCanet <scanet@libreduc.cc>
2019-07-20 12:39:30 +0200

    update libraries

--- portable/sketchbook/libraries/Adafruit_Motor_Shield_V2_Library/README.md
+++ portable/sketchbook/libraries/Adafruit_Motor_Shield_V2_Library/README.md
@@ -4,7 +4,7 @@
 
 This is the library for the Adafruit Motor Shield V2 for Arduino. It supports DC motors & Stepper motors with microstepping as well as stacking-support. It is *not* compatible with the V1 library!
 
-For use with the Motor Shield https://www.adafruit.com/products/1483
+For use with the Motor Shield https://www.adafruit.com/products/1438
 and Motor FeatherWing https://www.adafruit.com/product/2927
 
 Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
@@ -12,36 +12,3 @@ Adafruit invests time and resources providing this open source code, please supp
 Written by Limor Fried/Ladyada for Adafruit Industries.
 BSD license, check license.txt for more information.
 All text above must be included in any redistribution.
-
-
-<!-- START COMPATIBILITY TABLE -->
-
-## Compatibility
-
-MCU                | Tested Works | Doesn't Work | Not Tested  | Notes
------------------- | :----------: | :----------: | :---------: | -----
-Atmega328 @ 16MHz  |      X       |             |            | 
-Atmega328 @ 12MHz  |      X       |             |            | 
-Atmega32u4 @ 16MHz |      X       |             |            | 
-Atmega32u4 @ 8MHz  |      X       |             |            | 
-ESP8266            |             |             |     X       | 
-Atmega2560 @ 16MHz |      X       |             |            | 
-ATSAM3X8E          |             |      X       |            | 
-ATSAM21D           |             |      X       |            | 
-ATtiny85 @ 16MHz   |             |             |     X       | 
-ATtiny85 @ 8MHz    |             |             |     X       | 
-Intel Curie @ 32MHz |             |             |     X       | 
-STM32F2            |             |             |     X       | 
-
-  * ATmega328 @ 16MHz : Arduino UNO, Adafruit Pro Trinket 5V, Adafruit Metro 328, Adafruit Metro Mini
-  * ATmega328 @ 12MHz : Adafruit Pro Trinket 3V
-  * ATmega32u4 @ 16MHz : Arduino Leonardo, Arduino Micro, Arduino Yun, Teensy 2.0
-  * ATmega32u4 @ 8MHz : Adafruit Flora, Bluefruit Micro
-  * ESP8266 : Adafruit Huzzah
-  * ATmega2560 @ 16MHz : Arduino Mega
-  * ATSAM3X8E : Arduino Due
-  * ATSAM21D : Arduino Zero, M0 Pro
-  * ATtiny85 @ 16MHz : Adafruit Trinket 5V
-  * ATtiny85 @ 8MHz : Adafruit Gemma, Arduino Gemma, Adafruit Trinket 3V
-
-<!-- END COMPATIBILITY TABLE -->
--- portable/sketchbook/libraries/Adafruit_SleepyDog_Library/utility/WatchdogSAMD.cpp
+++ portable/sketchbook/libraries/Adafruit_SleepyDog_Library/utility/WatchdogSAMD.cpp
@@ -98,15 +98,17 @@ int WatchdogSAMD::enable(int maxPeriodMS, bool isForSleep) {
 
 #if defined(__SAMD51__)
     if(isForSleep) {
-        WDT->INTENSET.bit.EW   = 1;      // Enable early warning interrupt
-        WDT->CONFIG.bit.PER    = 0xB;    // Period = max
-        WDT->CONFIG.bit.WINDOW = bits;   // Set time of interrupt
-        WDT->CTRLA.bit.WEN     = 1;      // Enable window mode
+        WDT->INTFLAG.bit.EW      = 1;    // Clear interrupt flag
+        WDT->INTENSET.bit.EW     = 1;    // Enable early warning interrupt
+        WDT->CONFIG.bit.PER      = 0xB;  // Period = max
+        WDT->CONFIG.bit.WINDOW   = bits; // Set time of interrupt
+        WDT->EWCTRL.bit.EWOFFSET = 0x0;  // Early warning offset
+        WDT->CTRLA.bit.WEN       = 1;    // Enable window mode
         while(WDT->SYNCBUSY.reg);        // Sync CTRL write
     } else {
-        WDT->INTENCLR.bit.EW   = 1;      // Disable early warning interrupt
-        WDT->CONFIG.bit.PER    = bits;   // Set period for chip reset
-        WDT->CTRLA.bit.WEN     = 0;      // Disable window mode
+        WDT->INTENCLR.bit.EW     = 1;    // Disable early warning interrupt
+        WDT->CONFIG.bit.PER      = bits; // Set period for chip reset
+        WDT->CTRLA.bit.WEN       = 0;    // Disable window mode
         while(WDT->SYNCBUSY.reg);        // Sync CTRL write
     }
 
@@ -164,7 +166,6 @@ void WDT_Handler(void) {
 #else
     WDT->CTRL.bit.ENABLE = 0;        // Disable watchdog
     while(WDT->STATUS.bit.SYNCBUSY); // Sync CTRL write
-
 #endif
     WDT->INTFLAG.bit.EW  = 1;        // Clear interrupt flag
 }
@@ -179,7 +180,12 @@ int WatchdogSAMD::sleep(int maxPeriodMS) {
     // Don't fully power down flash when in sleep
     NVMCTRL->CTRLB.bit.SLEEPPRM = NVMCTRL_CTRLB_SLEEPPRM_DISABLED_Val;
 #endif
+#if defined(__SAMD51__)
+    PM->SLEEPCFG.bit.SLEEPMODE = 0x4;         // Standby sleep mode
+    while(PM->SLEEPCFG.bit.SLEEPMODE != 0x4); // Wait for it to take
+#else
     SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+#endif
 
     __DSB(); // Data sync to ensure outgoing memory accesses complete
     __WFI(); // Wait for interrupt (places device in sleep mode)
@@ -202,8 +208,8 @@ void WatchdogSAMD::_initialize_wdt() {
 #if defined(__SAMD51__)
     // SAMD51 WDT uses OSCULP32k as input clock now
     // section: 20.5.3
-    OSC32KCTRL->OSCULP32K.reg =
-      OSC32KCTRL_OSCULP32K_EN1K | OSC32KCTRL_OSCULP32K_EN32K;
+    OSC32KCTRL->OSCULP32K.bit.EN1K  = 1; // Enable out 1K (for WDT)
+    OSC32KCTRL->OSCULP32K.bit.EN32K = 0; // Disable out 32K
 
     // Enable WDT early-warning interrupt
     NVIC_DisableIRQ(WDT_IRQn);
@@ -212,6 +218,12 @@ void WatchdogSAMD::_initialize_wdt() {
     NVIC_EnableIRQ(WDT_IRQn);
 
     while(WDT->SYNCBUSY.reg);
+    
+    USB->DEVICE.CTRLA.bit.ENABLE = 0;         // Disable the USB peripheral
+    while(USB->DEVICE.SYNCBUSY.bit.ENABLE);   // Wait for synchronization
+    USB->DEVICE.CTRLA.bit.RUNSTDBY = 0;       // Deactivate run on standby
+    USB->DEVICE.CTRLA.bit.ENABLE = 1;         // Enable the USB peripheral
+    while(USB->DEVICE.SYNCBUSY.bit.ENABLE);   // Wait for synchronization
 #else
     // Generic clock generator 2, divisor = 32 (2^(DIV+1))
     GCLK->GENDIV.reg = GCLK_GENDIV_ID(2) | GCLK_GENDIV_DIV(4);
