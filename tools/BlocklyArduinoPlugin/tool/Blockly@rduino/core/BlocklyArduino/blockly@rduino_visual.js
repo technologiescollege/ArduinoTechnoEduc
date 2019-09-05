531acab3f3f6baf620988ef775eb540181f8b3be
SebCanet <scanet@libreduc.cc>
2019-09-06 01:03:28 +0200

    update B@

--- tools/BlocklyArduinoPlugin/tool/Blockly@rduino/core/BlocklyArduino/blockly@rduino_visual.js
+++ tools/BlocklyArduinoPlugin/tool/Blockly@rduino/core/BlocklyArduino/blockly@rduino_visual.js
@@ -279,14 +279,25 @@ BlocklyDuino.OnOffLine = function() {
 
 BlocklyDuino.ExampleWiring = function() {
 	var ExampleTest = BlocklyDuino.getStringParamFromUrl('url', '');
-	if (ExampleTest == '') {
+	var KitTest = BlocklyDuino.getStringParamFromUrl('card', '');
+	var KitTestResult = KitTest.slice(0, 3);
+	if ((ExampleTest == '')&&(KitTestResult != 'kit')) {
 		$("#btn_wiring").addClass("hidden");
 		$("#menu_21").addClass("hidden");
 	} else {
 		$("#btn_wiring").removeClass("hidden");
 		$("#menu_21").removeClass("hidden");
-		ExampleTest = ExampleTest.slice(0, -3);
-		$('#wiringModal_picture').prepend("<img src='" + ExampleTest + "jpg' id='wiringModalImg' width=100% height=auto/>");
+		if ((ExampleTest != '')&&(KitTestResult != 'kit')){
+			ExampleTest = ExampleTest.slice(0, -4);
+			$('#wiringModal_picture').prepend("<img src='" + ExampleTest + ".jpg' id='wiringModalImg' width=100% height=auto/>");
+			$('#arduino_card_miniPicture').attr('src', ExampleTest + '.jpg');
+			$('#arduino_card_picture').attr('src', ExampleTest + '_wiring.jpg');
+		}
+		if ((ExampleTest == '')&&(KitTestResult == 'kit')){
+			$('#wiringModal_picture').prepend("<img src='media/boards/" + KitTest + "_wiring.jpg' id='wiringModalImg' width=100% height=auto/>");
+			$("#btn_wiring").removeClass("hidden");
+			$("#menu_21").removeClass("hidden");
+		}
 	}
 };
 
