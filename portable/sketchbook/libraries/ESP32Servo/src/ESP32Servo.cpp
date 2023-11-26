2835a0aa731d94489d5d1522a1157425a161f50d
SebCanet <scanet@libreduc.cc>
2023-11-27 00:11:34 +0100

    update libraries

--- portable/sketchbook/libraries/ESP32Servo/src/ESP32Servo.cpp
+++ portable/sketchbook/libraries/ESP32Servo/src/ESP32Servo.cpp
@@ -128,7 +128,7 @@ if(
         // if you want anything other than default timer width, you must call setTimerWidth() before attach
         pwm.attachPin(this->pinNumber,REFRESH_CPS, this->timer_width );   // GPIO pin assigned to channel
         //Serial.println("Attaching servo : "+String(pin)+" on PWM "+String(pwm.getChannel()));
-        return 1;
+        return pwm.getChannel();
 }
 
 void Servo::detach()
--- portable/sketchbook/libraries/Keyboard/src/Keyboard.h
+++ portable/sketchbook/libraries/Keyboard/src/Keyboard.h
@@ -116,8 +116,10 @@ extern const uint8_t KeyboardLayout_en_US[];
 extern const uint8_t KeyboardLayout_es_ES[];
 extern const uint8_t KeyboardLayout_fr_FR[];
 extern const uint8_t KeyboardLayout_it_IT[];
+extern const uint8_t KeyboardLayout_pt_PT[];
 extern const uint8_t KeyboardLayout_sv_SE[];
 extern const uint8_t KeyboardLayout_da_DK[];
+extern const uint8_t KeyboardLayout_hu_HU[];
 
 // Low level key report: up to 6 keys and shift, ctrl etc at once
 typedef struct
